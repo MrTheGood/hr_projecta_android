c468f7346a2ed7fc8269966b00148b544bcb5588
MrTheGood <maarten@degoedeweb.nl>
2018-12-06 13:55:53 +0100

    Add button class

--- app/src/main/java/hh/corporation/urcompanion/Sketch.kt
+++ app/src/main/java/hh/corporation/urcompanion/Sketch.kt
@@ -12,6 +12,23 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
     private val state = State(State.Page.MAIN)
     private val background by lazy { loadImageAsset("app_background.png")?.apply { resize(g.width, g.height) } }
 
+    private val buttons by lazy {
+        listOf(
+                Button(
+                        x = width / 2f - 128f,
+                        y = height - 650f,
+                        width = 256f,
+                        height = 96f,
+                        radii = 8f,
+                        colorR = 216f,
+                        colorG = 27f,
+                        colorB = 96f,
+                        textColor = 255,
+                        onClick = state::throwDice
+                ).apply { text = "Throw Dice" }
+        )
+    }
+
     override fun settings() {
         size(w, h)
     }
@@ -24,35 +41,54 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
         background(background)
 
         if (state.page == Page.MAIN) {
+            buttons.forEach { it.draw(this) }
+
+            // region dice
             val diceSize = 200f
             val diceY = height - diceSize - 256f
             val dices = listOf(
-                    RectView(50f, diceY, diceSize, diceSize, 255, 16f),
-                    TextView(50f + diceSize / 2, diceY + diceSize / 3 * 2, 0, "${state.dice.x}"),
+                    RectView(50f, diceY, diceSize, diceSize, 16f),
+                    TextView(50f + diceSize / 2, diceY + diceSize / 3 * 2, "${state.dice.x}"),
 
-                    RectView(width / 2f - (diceSize / 2), diceY, diceSize, diceSize, 255, 16f),
-                    TextView(width / 2f, diceY + diceSize / 3 * 2, 0, "${state.dice.y}"),
+                    RectView(width / 2f - (diceSize / 2), diceY, diceSize, diceSize, 16f),
+                    TextView(width / 2f, diceY + diceSize / 3 * 2, "${state.dice.y}"),
 
-                    RectView(width - 50f - diceSize, diceY, diceSize, diceSize, 255, 16f),
-                    TextView(width - 50f - diceSize / 2, diceY + diceSize / 3 * 2, 0, "${state.dice.z}")
+                    RectView(width - 50f - diceSize, diceY, diceSize, diceSize, 16f),
+                    TextView(width - 50f - diceSize / 2, diceY + diceSize / 3 * 2, "${state.dice.z}")
             )
 
             textSize(100f)
             textAlign(CENTER)
-            for (dice in dices)
-                dice.draw(this)
-
+            dices.forEach { it.draw(this) }
 
             textSize(45f)
             fill(0)
             if (state.dice.sum == 0 || state.dice.sum == 3 || state.dice.sum == 6)
                 text("You may add a pawn!", width / 2f, height - 192f)
             text("Move ${state.dice.sum} step(s)", width / 2f, height - 128f)
+            // endregion
+        }
+    }
+
+    override fun touchStarted(event: TouchEvent) {
+        val p = event.getPointer(0)
+        buttons.forEach {
+            it.touching = it.collides(p.x, p.y)
+        }
+    }
+
+    override fun touchMoved(event: TouchEvent) {
+        val p = event.getPointer(0)
+        buttons.forEach {
+            it.touching = it.touching && it.collides(p.x, p.y)
         }
     }
 
     override fun touchEnded(event: TouchEvent) {
-        super.touchEnded(event)
-        state.throwDice()
+        buttons.forEach {
+            if (it.touching)
+                it.onClick()
+            it.touching = false
+        }
     }
 }
--- app/src/main/java/hh/corporation/urcompanion/View.kt
+++ app/src/main/java/hh/corporation/urcompanion/View.kt
@@ -10,8 +10,7 @@ abstract class View(
         var x: Float,
         var y: Float,
         var width: Float,
-        var height: Float,
-        var color: Int
+        var height: Float
 ) {
     fun collides(x: Float, y: Float) =
             x > this.x && x < this.x + width &&
@@ -20,17 +19,19 @@ abstract class View(
     abstract fun draw(applet: PApplet)
 }
 
-class RectView(
+open class RectView(
         x: Float,
         y: Float,
         width: Float,
         height: Float,
-        color: Int,
-        var radii: Float
-) : View(x, y, width, height, color) {
+        var radii: Float,
+        var colorR: Float = 255f,
+        var colorG: Float = 255f,
+        var colorB: Float = 255f
+) : View(x, y, width, height) {
 
     override fun draw(applet: PApplet): Unit = applet.run {
-        fill(color)
+        fill(colorR, colorG, colorB)
         rect(x, y, this@RectView.width, this@RectView.height, radii)
     }
 
@@ -39,13 +40,46 @@ class RectView(
 class TextView(
         x: Float,
         y: Float,
-        color: Int,
-        var text: String
-) : View(x, y, 0f, 0f, color) {
+        var text: String,
+        var color: Int = 0
+) : View(x, y, 0f, 0f) {
 
     override fun draw(applet: PApplet) = applet.run {
         fill(color)
         text(text, x, y)
     }
 
+}
+
+class Button(
+        x: Float,
+        y: Float,
+        width: Float,
+        height: Float,
+        radii: Float,
+        colorR: Float = 255f,
+        colorG: Float = 255f,
+        colorB: Float = 255f,
+        var textColor: Int = 0,
+        var onClick: () -> Unit = {}
+) : RectView(x, y, width, height, radii, colorR, colorG, colorB) {
+    var touching = false
+
+    var text: String = ""
+        set(value) {
+            field = value
+            textView = TextView(x + width / 2f, y + height / 1.5f, value, textColor)
+        }
+
+    private var textView = TextView(0f, 0f, "")
+
+    override fun draw(applet: PApplet) {
+        if (touching) applet.stroke(0)
+        else applet.noStroke()
+
+        applet.textSize(45f)
+        super.draw(applet)
+        textView.draw(applet)
+        applet.stroke(0)
+    }
 }

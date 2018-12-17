e52c5ae8523c7dba6bf0246e64cbed5f285f2a9c
MrTheGood <maarten@degoedeweb.nl>
2018-12-17 18:27:51 +0100

    Add documentation to the entire codebase.

---
+++ app/src/main/java/hh/corporation/urcompanion/Buttons.kt
@@ -0,0 +1,156 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
+package hh.corporation.urcompanion
+
+import processing.core.PApplet
+import processing.core.PImage
+
+/**
+ * Created by maartendegoede on 05/12/2018.
+ */
+
+/**
+ * This class represents a button.
+ *
+ * I really tried to remove as much classes as possible, but I couldn't get rid of these without making the code more complicated.
+ *
+ * A class isn't actually that hard.
+ * It is a blueprint for a variable with it's own internal state and it's own internal functions.
+ * Kind of like a set {"variable":value,"variable2":anotherValue,"anotherVariable":yetAnotherValue}, except it also has functions.
+ *
+ *
+ * Anyway, this class represents a single button.
+ */
+open class Button(
+        var x: Float, // x coordinate of the button
+        var y: Float, // y coordinate of the button
+        var w: Float, // button width
+        var h: Float, // button height
+        var radii: Float = 8f, // Corner radii for the button background rect
+        var colorR: Float = 216f, // background color values for the button background rect, default is the pink-ish accent colour
+        var colorG: Float = 27f,
+        var colorB: Float = 96f,
+        var textSize: Float = 45f, // Size for the included text, default set to 45
+        var textColor: Int = 255, // Color for the included text, default set to white
+        var text: String = "", // The text value for the included text (duh)
+        var strokeColor: Int = 0, // The colour of the stroke when the button is pressed
+        var pressing: Boolean = false, // True when the button is being hold down, false if not
+        var onClick: () -> Unit = {} // A lambda function, executed when the button is released
+) {
+    // Returns true if the touchX or touchY are within the button
+    private fun isTouching(touchX: Float, touchY: Float) =
+            touchX > x && touchX < x + w &&
+                    touchY > y && touchY < y + h
+
+    /**
+     * A touch has started.
+     * Pressing is set to true if the button is touched.
+     */
+    fun touchStarted(x: Float, y: Float) {
+        pressing = isTouching(x, y)
+    }
+
+    /**
+     * The touch position has moved.
+     * Pressing is set to false if the touch has stopped touching the button.
+     */
+    fun touchMoved(x: Float, y: Float) {
+        pressing = pressing && isTouching(x, y)
+    }
+
+    /**
+     * The touch has ended.
+     * Executes the onClick only if the button was still pressed.
+     */
+    fun touchEnded() {
+        if (pressing) onClick()
+        pressing = false
+    }
+
+    /**
+     * Draws a single button.
+     */
+    open fun draw(applet: PApplet) = applet.run {
+        if (pressing) {
+            // Draws a stroke and a slightly lighter button colour if the button is pressed
+            stroke(strokeColor)
+            fill(colorR + 10, colorG + 10, colorB + 10)
+        } else {
+            noStroke()
+            fill(colorR, colorG, colorB)
+        }
+
+        // Draw the background rectangle
+        rect(x, y, w, h, radii)
+
+        // Draw the button text
+        fill(textColor)
+        textSize(textSize)
+        text(text, x + w / 2f, y + h / 1.5f)
+    }
+}
+
+/**
+ * This class represents a button that is only an icon
+ *
+ * It 'inherits' all functions from the Button class (see above).
+ * But it overrides the draw function, so that the function in this class is executed instead of the one in the Button class.
+ * This because the functionality is the same. The only difference is in the appearance.
+ */
+open class IconButton(
+        val image: PImage,
+        x: Float,
+        y: Float,
+        w: Float = image.width.toFloat(),
+        h: Float = image.height.toFloat(),
+        onClick: () -> Unit = {}
+) : Button(x, y, w, h, onClick = onClick) {
+
+    init {
+        image.resize(w.toInt(), h.toInt())
+    }
+
+    override fun draw(applet: PApplet) = applet.run {
+        image(image, x, y)
+    }
+}
+
+/**
+ * This class represents a button with an icon and a circular background.
+ *
+ * It 'inherits' all functions from the Button class (see above).
+ * But it overrides the draw function, so that the function in this class is executed instead of the one in the Button class.
+ * This because the functionality is the same. The only difference is in the appearance.
+ */
+class CircularIconButton(
+        val image: PImage,
+        x: Float,
+        y: Float,
+        w: Float = image.width.toFloat(),
+        h: Float = image.height.toFloat(),
+        colorR: Float = 216f,
+        colorG: Float = 27f,
+        colorB: Float = 96f,
+        val padding: Float,
+        onClick: () -> Unit = {}
+) : Button(x, y, (w + padding) * 2, (h + padding) * 2, colorR = colorR, colorG = colorG, colorB = colorB, onClick = onClick) {
+
+    init {
+        image.resize(w.toInt(), h.toInt())
+    }
+
+    override fun draw(applet: PApplet) = applet.run {
+        if (pressing) {
+            // Draws a stroke and a slightly lighter button colour if the button is pressed
+            stroke(strokeColor)
+            fill(colorR + 10, colorG + 10, colorB + 10)
+        } else {
+            noStroke()
+            fill(colorR, colorG, colorB)
+        }
+
+        ellipse(x + w / 4, y + h / 4, w, h)
+
+        image(image, x, y)
+    }
+}
--- app/src/main/java/hh/corporation/urcompanion/Sketch.kt
+++ app/src/main/java/hh/corporation/urcompanion/Sketch.kt
@@ -15,15 +15,27 @@ const val PAGE_CARD = "CARD"
 const val CARD_PLUS = "PLUS"
 const val CARD_CIRCLE = "CIRCLE"
 
-
-class Sketch(private val w: Int, private val h: Int) : PApplet() {
+class Sketch(private val pw: Int, private val ph: Int) : PApplet() {
+    /**
+     * The next couple variables represent the state.
+     * These include:
+     * - The current page, either MAIN or CARD
+     * - The current dice value
+     * - The currently drawn card
+     * - Whether the card is showing or not
+     * - The type of the current card, either PLUS or CIRCLE
+     */
     private var currentPage = PAGE_MAIN
     private var currentDice = listOf(0, 0, 0)
 
     private var currentCardType = CARD_PLUS
     private var currentCard = ""
-    private var showingCard = false
+    private var currentCardShown = false
 
+    /**
+     * These lists represent all possible cards.
+     * They are of type [LinkedList], which is pretty much just an ordered list with some extra functionality
+     */
     private val circleCards = LinkedList(listOf(
             "Throw off any opponent pawn",
             "Throw off any opponent pawn",
@@ -78,12 +90,23 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
             "Let an opponent player\ndraw a + card"
     ).shuffled())
 
+    /**
+     * The next couple variables are all necessary images.
+     * You can ignore the "by lazy" for now. [loadImageAsset] loads the image from the disk.
+     */
     private val background by lazy { loadImageAsset("app_background.png")?.apply { resize(g.width, g.height) } }
     private val logo by lazy { loadImageAsset("ur.png") }
     private val card by lazy { loadImageAsset("card_background.png") }
     private val circleCard by lazy { loadImageAsset("card_background_circle.png") }
     private val plusCard by lazy { loadImageAsset("card_background_plus.png") }
-    private val appBarButton by lazy {
+
+    /**
+     * The next couple variables represent all buttons.
+     * Each button is an instance of the [Button] class, which is nothing more than a variable which holds the coordinates and size of the buttons.
+     *
+     * More explanation on the buttons can be found in Buttons.kt
+     */
+    private val rulebookButton by lazy {
         IconButton(
                 image = loadImageAsset("ic_book.png")!!,
                 x = width - 104f,
@@ -93,7 +116,6 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
                 onClick = { openPdf("ur_manual.pdf") }
         )
     }
-
     private val cardPageButtons by lazy {
         listOf(IconButton(
                 image = loadImageAsset("ic_back.png")!!,
@@ -109,7 +131,7 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
                 padding = 4f,
                 w = 72f,
                 h = 72f,
-                onClick = { showingCard = true }
+                onClick = { currentCardShown = true }
         ), IconButton(
                 image = loadImageAsset("ic_next.png")!!,
                 x = (width - 72f) / 1.3f,
@@ -145,23 +167,13 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
     }
 
     override fun settings() {
-        size(w, h)
+        // Sets the size to whatever the screen size is. Required because fullscreen() sucks.
+        size(pw, ph)
     }
 
-    private fun drawRect(
-            x: Float,
-            y: Float,
-            width: Float,
-            height: Float,
-            radii: Float,
-            colorR: Float = 255f,
-            colorG: Float = 255f,
-            colorB: Float = 255f
-    ) {
-        fill(colorR, colorG, colorB)
-        rect(x, y, width, height, radii)
-    }
-
+    /**
+     * A simple function to more easily draw a dice.
+     */
     private fun drawDice(
             x: Float,
             y: Float,
@@ -169,9 +181,11 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
             value: Int,
             radii: Float = 16f
     ) {
+        stroke(0)
         textSize(100f)
         textAlign(CENTER)
-        drawRect(x, y, size, size, radii)
+        fill(255)
+        rect(x, y, size, size, radii)
         fill(0)
         text(value, x + size / 2, y + size / 3 * 2)
     }
@@ -180,19 +194,22 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
         fill(255f, 255f, 255f)
         background(background)
 
-        image(logo, width / 2f - 88f, 56f, 176f, 160f)
-        appBarButton.draw(this)
+        image(logo, width / 2f - 88f, 56f, 176f, 160f) // Draw the logo
+        rulebookButton.draw(this) // Draw the rulebook button using the draw function defined in Buttons.kt
 
         when (currentPage) {
-            PAGE_MAIN -> drawMainPage()
-            PAGE_CARD -> drawCardsPage()
+            PAGE_MAIN -> drawMainPage() // execute drawMainPage() if the current page is the main page
+            PAGE_CARD -> drawCardsPage() // execute drawCardsPage() if the current page is the cards page
         }
     }
 
+    /**
+     * Draws the main page.
+     */
     private fun drawMainPage() {
         fill(0)
         text("Draw a card", width / 2f, 640f)
-        mainPageButtons.forEach { it.draw(this) }
+        mainPageButtons.forEach { it.draw(this) } // Draws all buttons using the draw function defined in Buttons.kt
 
         // Draw the dice
         val diceSize = 200f
@@ -209,8 +226,11 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
         text("Move $sum step(s)", width / 2f, height - 128f)
     }
 
+    /**
+     * Draws the cards page
+     */
     private fun drawCardsPage() {
-        cardPageButtons.forEach { it.draw(this) }
+        cardPageButtons.forEach { it.draw(this) } // Draws all buttons using the draw function defined in Buttons.kt
 
         textAlign(CENTER)
         textSize(45f)
@@ -218,7 +238,9 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
         image(card, 64f, 224f, width - 128f, height - 480f)
         text(currentCard, width / 2f, height / 2f)
 
-        if (!showingCard) {
+        // Draw the back of the current card on top of the text if the card shouldn't be shown yet
+        // when is just a fancy if-else_if statement.
+        if (!currentCardShown) {
             when (currentCardType) {
                 CARD_CIRCLE -> image(circleCard, 64f, 224f, width - 128f, height - 480f)
                 CARD_PLUS -> image(plusCard, 64f, 224f, width - 128f, height - 480f)
@@ -226,14 +248,20 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
         }
     }
 
+    /**
+     * Go to the main page if the Android back button is pressed while on the cards page
+     */
     override fun onBackPressed() {
         if (currentPage == PAGE_CARD) currentPage = PAGE_MAIN
         else super.onBackPressed()
     }
 
+    /**
+     * Passes the touchStarted event on to the visible buttons
+     */
     override fun touchStarted(event: TouchEvent) {
         val p = event.getPointer(0)
-        appBarButton.touchStarted(p.x, p.y)
+        rulebookButton.touchStarted(p.x, p.y)
 
         when (currentPage) {
             PAGE_MAIN -> mainPageButtons.forEach { it.touchStarted(p.x, p.y) }
@@ -241,9 +269,12 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
         }
     }
 
+    /**
+     * Passes the touchMoved event on to the visible buttons
+     */
     override fun touchMoved(event: TouchEvent) {
         val p = event.getPointer(0)
-        appBarButton.touchMoved(p.x, p.y)
+        rulebookButton.touchMoved(p.x, p.y)
 
         when (currentPage) {
             PAGE_MAIN -> mainPageButtons.forEach { it.touchMoved(p.x, p.y) }
@@ -251,8 +282,11 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
         }
     }
 
+    /**
+     * Passes the touchEnded event on to the visible buttons
+     */
     override fun touchEnded(event: TouchEvent) {
-        appBarButton.touchEnded()
+        rulebookButton.touchEnded()
 
         when (currentPage) {
             PAGE_MAIN -> mainPageButtons.forEach { it.touchEnded() }
@@ -260,14 +294,26 @@ class Sketch(private val w: Int, private val h: Int) : PApplet() {
         }
     }
 
+    /**
+     * Sets the currentDice list to a new list containing 3 random integers between 0..2
+     */
     private fun throwDice() {
         currentDice = Random().run { listOf(nextInt(3), nextInt(3), nextInt(3)) }
     }
 
+    /**
+     * Sets currentCard to the card from top card of the specified list, and also sends the card to the bottom of the list.
+     */
     private fun drawCard(cardType: String = currentCardType) {
-        when (cardType) {
-            CARD_PLUS -> plusCards.pop().also { plusCards.add(it) }
-            CARD_CIRCLE -> circleCards.pop().also { circleCards.add(it) }
+        currentPage = PAGE_CARD
+
+        currentCardType = cardType
+        currentCardShown = false
+
+        currentCard = when (cardType) {
+            CARD_PLUS -> plusCards.pop().also { plusCards.add(it) } // Gets the top card of the plusCards list and also adds it to the bottom
+            CARD_CIRCLE -> circleCards.pop().also { circleCards.add(it) } // Gets the top card of the circleCards list and also adds it to the bottom
+            else -> throw RuntimeException("cardType $cardType not found") // Crashes the app when looking for a card type that doesn't exist.
         }
     }
 }
